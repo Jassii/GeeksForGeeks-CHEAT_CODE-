431b05152f43cd044128fcfb86c055967b34acea
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-07-05 21:32:31 +0530

    Create Search in a matrix.java

---
+++ Matrix/Search in a matrix.java
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            
+            int mat[][] = new int[n][m];
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                  mat[i][j] = sc.nextInt();
+            }
+            
+            int x = sc.nextInt();
+            
+            System.out.println (new Sol().matSearch(mat, n, m, x));
+        }
+        
+    }
+}// } Driver Code Ends
+
+
+class Sol
+{
+    public static int matSearch(int mat[][], int N, int M, int X)
+    {
+        // your code here
+        /*  Brute Force Approach..
+        int i,j;
+        for(i=0;i<N;i++)
+        {
+            for(j=0;j<M;j++)
+            {
+                if(mat[i][j]==X)
+                {
+                    return 1;
+                }
+            }
+        }
+        return 0;*/
+        
+        int i=0,j=M-1;
+        while(i<N && j>=0)
+        {
+            int val = mat[i][j];
+            if(val==X)
+            {
+                return 1;
+            }
+            else if(val<X)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        return 0;
+    }
+}
