ead43f9635630c47fd99dfdeb43b358873f02c4f
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-07-06 18:52:46 +0530

    Create Reverse words in a given string.java

---
+++ String/Reverse words in a given string.java
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.reverseWords(s));
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+class Solution 
+{
+    //Function to reverse words in a given string.
+    String reverseWords(String S)
+    {
+        // code here 
+        
+        String str[] =  S.split("[.]"); //this will separate the data.
+        String res = ""; //final string.
+        int i;
+        for(i=str.length-1;i>=0;i--)
+        {
+            if(i!=0)
+            {
+                res = res + str[i] + ".";
+            }
+            else
+            {
+                res = res+str[i];
+            }
+        }
+        return res;
+    }
+}
