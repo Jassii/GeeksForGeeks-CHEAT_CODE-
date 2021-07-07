6c82efccbb3cc8e066e647d83f7ae1d1df17e116
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-07-07 21:47:44 +0530

    Create Non Repeating Numbers.java

---
+++ Bit Masking/Non Repeating Numbers.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] v = new int[2 * n + 2];
+            for(int i = 0; i < 2 * n + 2; i++)
+            {
+                v[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            int[] ans = ob.singleNumber(v);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] singleNumber(int[] nums)
+    {
+        // Code here
+        
+        int arr[] = new int[2];
+        Map<Integer,Integer> M = new HashMap<>();
+        int i;
+        for(i=0;i<nums.length;i++)
+        {
+            if(M.containsKey(nums[i]))
+            {
+                M.put(nums[i],M.get(nums[i])+1);
+            }
+            else
+            {
+                M.put(nums[i],1);
+            }
+        }
+        int k=0;
+        for(Map.Entry<Integer,Integer> entry:M.entrySet())
+        {
+            if(entry.getValue()==1)
+            {
+                arr[k]=entry.getKey();
+                k++;
+            }
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
