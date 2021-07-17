dc6a529ab61afdaadde671e37aae953727ddd560
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-07-17 19:52:11 +0530

    Create Inorder Successor in BST .java

---
+++ Binary Search Tree/Inorder Successor in BST .java
@@ -0,0 +1,180 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str)
+    {
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+
+    public static Node search(Node root,int x){
+		if(root==null||root.data==x)
+			return root;
+		if(root.data>x)
+			return search(root.left,x);
+		return search(root.right,x);
+	}
+
+	public static void main (String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            
+            int t=Integer.parseInt(br.readLine());
+            
+            while(t > 0){
+                String s = br.readLine();
+                Node root = buildTree(s);
+                
+                int k=Integer.parseInt(br.readLine());
+                Node kNode = search(root,k);
+                
+                Solution g = new Solution();
+                Node x = g.inorderSuccessor(root,kNode);
+                
+                if(x==null) System.out.println("-1");
+                else System.out.println(x.data);
+                t--;
+            }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*Complete the function below
+Node is as follows:
+class Node{
+	int data;
+	Node left,right;
+	Node(int d){
+		data=d;
+		left=right=null;
+	}
+}
+*/
+class Solution
+{
+    // returns the inorder successor of the Node x in BST (rooted at 'root')
+	public Node inorderSuccessor(Node root,Node x)
+    {
+        //add code here.
+    
+    
+    /**************************************************BRUTE FORCE APPROACH*********************************************/
+        if(root==null)
+        {
+            return null;
+        }
+        
+        ArrayList<Integer> l = new ArrayList<>(); //will store the inorder.
+        //ArrayList<Integer> in = new ArrayList<>();
+        l = inorder(root,l);
+        //System.out.println("Inorder successor "+l);
+        
+        int i;
+        for(i=0;i<l.size();i++)
+        {
+            if(l.get(i)==x.data)  //it it matches then the next value will be sucessor
+            {
+                int j = i+1;
+                if(j<l.size())  //if it exists..
+                {
+                    Node value = new Node(l.get(j));
+                    return value;
+                }
+                else  //it does not exits in the whole BST..
+                {
+                    break;
+                }
+            }
+        }
+        return null;  
+    }
+    public static ArrayList<Integer> inorder(Node root,ArrayList<Integer> l)
+    {
+        if(root==null)
+        {
+            return new ArrayList<>();  //return empty list,
+        }
+
+        inorder(root.left,l);
+        l.add(root.data);
+        inorder(root.right,l);
+        
+        return l;
+      
+      /***********************************************************************************************/
+      //not complete...
+      
+    }
+}
