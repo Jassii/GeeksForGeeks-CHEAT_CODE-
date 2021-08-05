9452bd5a0f19eb3d023f193d14e7b95499f7db1d
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-08-05 22:32:50 +0530

    Create Remove duplicates from an unsorted linked list.java

---
+++ LinkedList/Remove duplicates from an unsorted linked list.java
@@ -0,0 +1,171 @@
+// Given an unsorted linked list of N nodes. The task is to remove duplicate elements from this unsorted Linked List. When a value appears in multiple nodes, the node which appeared first should be kept, all others duplicates are to be removed.
+
+// Example 1:
+
+// Input:
+// N = 4
+// value[] = {5,2,2,4}
+// Output: 5 2 4
+// Explanation:Given linked list elements are
+// 5->2->2->4, in which 2 is repeated only.
+// So, we will delete the extra repeated
+// elements 2 from the linked list and the
+// resultant linked list will contain 5->2->4
+// Example 2:
+
+// Input:
+// N = 5
+// value[] = {2,2,2,2,2}
+// Output: 2
+// Explanation:Given linked list elements are
+// 2->2->2->2->2, in which 2 is repeated. So,
+// we will delete the extra repeated elements
+// 2 from the linked list and the resultant
+// linked list will contain only 2.
+// Your Task:
+// You have to complete the method removeDuplicates() which takes 1 argument: the head of the linked list.  Your function should return a pointer to a linked list with no duplicate element.
+
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(N)
+
+
+// { Driver Code Starts
+/* package whatever; // don't place package name! */
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+
+class Remove_Duplicate_From_LL
+{
+    Node head;  
+    Node temp;
+	public void addToTheLast(Node node) 
+	{
+
+	  if (head == null) 
+	  {
+	    head = node;
+	    temp = node;
+	  }
+	  else{
+	      temp.next = node;
+	      temp = node;
+	  }
+	}
+
+      void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+       
+        
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		  
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			Remove_Duplicate_From_LL llist = new Remove_Duplicate_From_LL();
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+		//llist.printList();	
+        Solution g = new Solution();
+		llist.head = g.removeDuplicates(llist.head);
+		llist.printList();
+		
+        t--;		
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* The structure of linked list is the following
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+*/
+
+class Solution
+{
+    //Function to remove duplicates from unsorted linked list.
+    public Node removeDuplicates(Node head) 
+    {
+         // Your code here
+       
+       //MY APPROACH......................................................................
+        Node curr = head;
+        Map<Integer,Integer> M = new HashMap<>();
+        M.put(curr.data,1);
+        
+        while(curr.next!=null)
+        {
+            int val = curr.next.data;
+            if(M.containsKey(val))
+            {
+                curr.next = curr.next.next;
+            }
+            else
+            {
+                M.put(val,1);
+                curr = curr.next;
+            }
+        }
+        
+        return head;
+       
+       //ANOTHER APPROACH........................................................................................
+        Node curr = head;
+        Map<Integer,Integer> M = new HashMap<>();
+        M.put(curr.data,1);
+        
+        Node prev = curr;
+        curr = curr.next;
+        while(curr!=null)
+        {
+            if(M.containsKey(curr.data))
+            {
+                prev.next = curr.next;
+                curr.next = null;  //free
+            }
+            else
+            {
+                M.put(curr.data,1);
+                prev=curr;
+            }
+            curr = prev.next;
+        }
+        return head;
+    }
+}
