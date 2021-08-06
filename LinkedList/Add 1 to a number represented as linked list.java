ad989ca0a6c6a91d59132ad4582eea1a281f5a7e
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-08-06 20:10:15 +0530

    Create Add 1 to a number represented as linked list.java

---
+++ LinkedList/Add 1 to a number represented as linked list.java
@@ -0,0 +1,143 @@
+// A number N is represented in Linked List such that each digit corresponds to a node in linked list. You need to add 1 to it.
+
+// Example 1:
+
+// Input:
+// LinkedList: 4->5->6
+// Output: 457 
+// Example 2:
+
+// Input:
+// LinkedList: 1->2->3
+// Output: 124 
+// Your Task:
+// Your task is to complete the function addOne() which takes the head of the linked list as the only argument and returns the head of the modified linked list. The driver code prints the number.
+// Note: The head represents the left-most digit of the number.
+
+// Expected Time Complexity: O(N).
+// Expected Auxiliary Space: O(1).
+
+/*********************************************************************************************************************************************/
+
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    public static Node addOne(Node head) 
+    { 
+        //code here.
+        
+        //reverse the list..
+        Node curr = head;
+        Node prev = null;
+        Node next = null;
+        while(curr!=null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        //Now the linked list has been reversed..
+        
+        int carry=0;
+        Node temp = head;
+        while(temp!=null)
+        {
+            if(temp.data!=9)
+            {
+                int val = temp.data+1;  //increase it by one..
+                temp.data = val;  //update the data..
+                carry=0;
+                break;
+            }
+            else
+            {
+               carry=1;
+               temp.data=0;
+               temp = temp.next;
+            }
+        }
+        
+        //now again reverse the linked list.
+        curr = head;
+        prev = null;
+        next = null;
+        while(curr!=null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        
+        
+        if(carry==1)
+        {
+            Node C = new Node(carry); //created a new node..
+            C.next = head;
+            head = C;
+        }
+        return head;
+        
+    }
+}
