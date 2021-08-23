b349da2bc8b2ae966cf2fcac9220db7d95b94b2c
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-08-23 20:12:48 +0530

    Create Preorder Traversal(Iterative Approach).java

---
+++ Binary Tree/Preorder Traversal(Iterative Approach).java
@@ -0,0 +1,186 @@
+// Given a binary tree, find its preorder traversal.
+
+// Example 1:
+
+// Input:
+//         1      
+//       /          
+//     4    
+//   /    \   
+// 4       2
+// Output: 1 4 4 2 
+// Example 2:
+
+// Input:
+//        6
+//      /   \
+//     3     2
+//      \   / 
+//       1 2
+// Output: 6 3 1 2 2 
+
+// Your Task:
+// You just have to complete the function preorder() which takes the root node of the tree as input and returns an array containing the preorder traversal of the tree.
+
+// Expected Time Complexity: O(N).
+// Expected Auxiliary Space: O(N).
+
+
+// { Driver Code Starts
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+                ArrayList<Integer> res = BinaryTree.preorder(root);
+                for (Integer num : res)
+                    System.out.print(num + " ");
+                System.out.println();
+                t--;
+            
+        }
+    }
+   
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+// class Node{
+//     int data;
+//     Node left,right;
+//     Node(int d){
+//         data=d;
+//         left=right=null;
+//     }
+// }
+
+class BinaryTree
+{
+    //Function to return a list containing the preorder traversal of the tree.
+    static ArrayList<Integer> preorder(Node root)
+    {
+        // Code here
+        
+        //list containing the preorder traversal of the binary tree.
+        //no need to define the array list as static..
+        ArrayList<Integer> pre = new ArrayList<>();
+        
+        if(root==null)
+        {
+            return pre;
+        }
+        
+        
+        Stack<Node> S = new Stack<>();
+        Node curr = root;
+        S.push(curr);
+        while(!S.isEmpty())
+        {
+            curr = S.pop();
+            pre.add(curr.data);
+            
+            if(curr.right!=null)
+            {
+                S.push(curr.right);
+            }
+            if(curr.left!=null)
+            {
+                S.push(curr.left);
+            }
+        }
+        return pre;
+    }
+}
