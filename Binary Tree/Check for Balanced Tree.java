ddfd163cdb9a52e010c8f40e034545851c6bab7d
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-08-25 20:17:52 +0530

    Update Check for Balanced Tree.java

--- Binary Tree/Check for Balanced Tree.java
+++ Binary Tree/Check for Balanced Tree.java
@@ -1,3 +1,54 @@
+// Given a binary tree, find if it is height balanced or not. 
+// A tree is height balanced if difference between heights of left and right subtrees is not more than one for all nodes of tree. 
+
+// A height balanced tree
+//         1
+//      /     \
+//    10      39
+//   /
+// 5
+
+// An unbalanced tree
+//         1
+//      /    
+//    10   
+//   /
+// 5
+
+// Example 1:
+
+// Input:
+//       1
+//     /
+//    2
+//     \
+//      3 
+// Output: 0
+// Explanation: The max difference in height
+// of left subtree and right subtree is 2,
+// which is greater than 1. Hence unbalanced
+// Example 2:
+
+// Input:
+//        10
+//      /   \
+//     20   30 
+//   /   \
+//  40   60
+// Output: 1
+// Explanation: The max difference in height
+// of left subtree and right subtree is 1.
+// Hence balanced. 
+// Your Task:
+// You don't need to take input. Just complete the function isBalanced() that takes root node as parameter and returns true, if the tree is balanced else returns false.
+
+// Constraints:
+// 1 <= Number of nodes <= 105
+// 0 <= Data of a node <= 106
+
+// Expected time complexity: O(N)
+// Expected auxiliary space: O(h) , where h = height of tree
+
 // { Driver Code Starts
 //Initial Template for Java
 
@@ -129,6 +180,7 @@ class Node
 class Tree
 {
     
+	/*******************************************************FIRST METHOD******************************************************************/
     //Function to check whether a binary tree is balanced or not.
     boolean isBalanced(Node root)
     {
@@ -156,5 +208,65 @@ class Tree
         int rh = height(node.right);
         int h = Math.max(lh,rh);  //maximum height..
         return h+1;
+    }
+	/****************************SECOND METHOD********************************************************************************************************/
+	
+	boolean isBalanced(Node root)
+    {
+        // Your code here
+        
+        //for every node difference b/w the left and right subtree is atmost 1.
+        
+        //base condition
+        if(root==null)
+        {
+            return false;
+        }
+        
+        //traverse the Binary Tree..
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty())
+        {
+        
+            Node curr = q.poll();
+            
+            //for current node find the max left height and the right height..
+            int lh = height(curr.left);  //left height..
+            int rh = height(curr.right);  //right height..
+            
+            //find the difference.
+            int dif = Math.abs(lh-rh);
+            
+            //for unbalanced binary tree..
+            if(dif>1)
+            {
+                return false;
+            }
+        
+            if(curr.left!=null)
+            {
+                q.add(curr.left);
+            }
+            if(curr.right!=null)
+            {
+                q.add(curr.right);
+            }
+        }
+        return true;
+    }
+    
+    int height(Node node)
+    {
+        if(node==null)
+        {
+            return 0;
+        }
+        
+        int l = height(node.left); //left subtree height..
+        int r = height(node.right); //right subtree height..
+        int h = Math.max(l,r)+1;
+        return h;
     }
 }
