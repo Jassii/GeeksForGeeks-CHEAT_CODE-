75e5d41f09331170837da9bb785a75c26ab8f5d7
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-08-25 19:03:32 +0530

    Create Bottom View of Binary Tree.java

---
+++ Binary Tree/Bottom View of Binary Tree.java
@@ -0,0 +1,241 @@
+// Given a binary tree, print the bottom view from left to right.
+// A node is included in bottom view if it can be seen when we look at the tree from bottom.
+
+//                       20
+//                     /    \
+//                   8       22
+//                 /   \        \
+//               5      3       25
+//                     /   \      
+//                   10    14
+
+// For the above tree, the bottom view is 5 10 3 14 25.
+// If there are multiple bottom-most nodes for a horizontal distance from root, then print the later one in level traversal. For example, in the below diagram, 3 and 4 are both the bottommost nodes at horizontal distance 0, we need to print 4.
+
+//                       20
+//                     /    \
+//                   8       22
+//                 /   \     /   \
+//               5      3 4     25
+//                      /    \      
+//                  10       14
+
+// For the above tree the output should be 5 10 4 14 25.
+ 
+
+// Example 1:
+
+// Input:
+//        1
+//      /   \
+//     3     2
+// Output: 3 1 2
+// Explanation:
+// First case represents a tree with 3 nodes
+// and 2 edges where root is 1, left child of
+// 1 is 3 and right child of 1 is 2.
+
+// Thus nodes of the binary tree will be
+// printed as such 3 1 2.
+// Example 2:
+
+// Input:
+//          10
+//        /    \
+//       20    30
+//      /  \
+//     40   60
+// Output: 40 20 60 30
+// Your Task:
+// This is a functional problem, you don't need to care about input, just complete the function bottomView() which takes the root node of the tree as input and returns an array containing the bottom view of the given tree.
+
+// Expected Time Complexity: O(N).
+// Expected Auxiliary Space: O(N).
+  
+// { Driver Code Starts
+//Initial Template for Java
+
+
+//Contributed by Sudarshan Sharma
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data; //data of the node
+    int hd; //horizontal distance of the node
+    Node left, right; //left and right references
+ 
+    // Constructor of tree node
+    public Node(int key)
+    {
+        data = key;
+        hd = Integer.MAX_VALUE;
+        left = right = null;
+    }
+}
+
+
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	        Solution ob = new Solution();
+			    ArrayList <Integer> res = ob.bottomView(root);
+			    for (Integer num : res) System.out.print (num + " ");
+		     	System.out.println();
+	        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to return a list containing the bottom view of the given tree.
+    public ArrayList <Integer> bottomView(Node root)
+    {
+        // Code here
+        
+        ArrayList <Integer> bottom = new ArrayList <Integer>(); //bottom array list.
+        
+        //base condition.
+        if(root==null)
+        {
+            return bottom;
+        }
+        
+        //create a class Q which will have node,height..
+        class Q
+        {
+            int h;
+            Node node;
+            Q(int h,Node node)
+            {
+                this.h = h;
+                this.node = node;
+            }
+        }
+        
+        //create a tree map..which will store key,value pair in sorted manner
+        //based on key.
+        TreeMap<Integer,Node> M = new TreeMap<>();
+        
+        //creating a queue which will help in traversing the binary tree.
+        Queue<Q> q = new LinkedList<>();
+        
+        q.add(new Q(0,root));
+        //traverse the BT untill the queue is empty
+        while(!q.isEmpty())
+        {
+            Q curr = q.poll();
+            if(!M.containsKey(curr.h)) //if it does not contains key
+            {
+                M.put(curr.h,curr.node);
+            }
+            else //if key is already there.then simply update the key and the node
+            {
+                M.put(curr.h,curr.node);
+            }
+            
+            if(curr.node.left!=null)
+            {
+                q.add(new Q(curr.h-1,curr.node.left)); //creating a object for left 
+                //subtree and enquing in the queue.
+            }
+            if(curr.node.right!=null)
+            {
+                q.add(new Q(curr.h+1,curr.node.right));//creating a object for right 
+                //subtree and enquing in the queue.
+            }
+            
+        }
+        //now i feel that the tree map will be having all the bottom nodes in 
+        // sorted order of the key..
+        for(Map.Entry<Integer,Node> entry:M.entrySet())
+        {
+            bottom.add(entry.getValue().data);
+        }
+        return bottom;
+    }
+}
